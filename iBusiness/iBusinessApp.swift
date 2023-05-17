ad938f7733fed7bddb9106e9e8e3c7369ec2cb40
Aldino Efendi <aldino.efendi@icloud.com>
2023-05-17 11:31:32 +0700

    Initial Commit

---
+++ iBusiness/iBusinessApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBusinessApp.swift
+//  iBusiness
+//
+//  Created by Aldino Efendi on 2023-05-17.
+//
+
+import SwiftUI
+
+@main
+struct iBusinessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
